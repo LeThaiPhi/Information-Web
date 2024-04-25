84517beb7487f694a31ffc4a7d36e95ac012d570
ThaoVy <144774143+LeThaiPhi@users.noreply.github.com>
2024-04-25 22:09:39 +0700

    25/4

---
+++ InformationsMVC/InformationsMVC/Controllers/AccountController.cs
@@ -0,0 +1,39 @@
+﻿using InformationsMVC.Data;
+using InformationsMVC.Models;
+using Microsoft.AspNetCore.Mvc;
+namespace InformationsMVC.Controllers
+{
+    public class AccountController : Controller
+
+    {
+        private readonly InformationsMVCContext _context;
+
+        public AccountController(InformationsMVCContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Login()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Login(User _userFromPage)
+        {
+            var _user = _context.User.Where(m => m.Email == _userFromPage.Email && m.Password == _userFromPage.Password).FirstOrDefault();
+            if (_user == null)
+            {
+                ViewBag.LoginStatus = 0;
+            }
+            else
+            {
+                return RedirectToAction("Index", "Account");
+            }
+            return View();
+        }
+    }
+}
---
+++ InformationsMVC/InformationsMVC/Data/InformationsMVCContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace InformationsMVC.Data
+{
+    public class InformationsMVCContext : DbContext
+    {
+        public InformationsMVCContext(DbContextOptions<InformationsMVCContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<InformationsMVC.Models.User> User { get; set; } = default!;
+    }
+}
